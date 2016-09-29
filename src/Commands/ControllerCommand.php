65603e9cdc221b82a39b81c05df04e304f4e35ab
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-09-29 13:54:56 +0200

    Add validators option to make controller command

--- src/Commands/ControllerCommand.php
+++ src/Commands/ControllerCommand.php
@@ -8,6 +8,7 @@ use Nwidart\Modules\Traits\ModuleCommandTrait;
 class ControllerCommand extends GeneratorCommand
 {
     use ModuleCommandTrait;
+    use UsesImportsAndTraits;
     
     /**
      * The name of argument being used.
@@ -25,7 +26,8 @@ class ControllerCommand extends GeneratorCommand
                             {resource : The singular name of the resource}
                             {module? : The name of the module to create the controller in}
                             {--plain : Create an empty controller without CRUD methods}
-                            {--repository : Implement the use of a repository}';
+                            {--repository : Implement the use of a repository}
+                            {--validators : Implement the use of form request validators}';
     
     /**
      * The console command description.
@@ -33,11 +35,6 @@ class ControllerCommand extends GeneratorCommand
      * @var string
      */
     protected $description = 'Generate a new restful resource controller in the given module.';
-    
-    /**
-     * @var array
-     */
-    protected $imports = [];
     
     /**
      * Get the destination file path.
@@ -60,20 +57,49 @@ class ControllerCommand extends GeneratorCommand
     {
         $module = $this->laravel['modules']->findOrFail($this->getFullyQualifiedName());
         
+        $resource = strtolower(str_plural($this->argument($this->argumentName)));
+        
+        $replacements = [
+            'CLASS_NAMESPACE' => $this->getClassNamespace($module),
+            'CLASS' => $this->getControllerName(),
+            'MODULE' => $this->getFullyQualifiedName(),
+            
+            'RESOURCE' => $resource,
+            'RESOURCE_SINGULAR' => strtolower($this->argument($this->argumentName)),
+            
+            'REPOSITORY' => class_basename($this->getRepositoryClassName()),
+            'NAMESPACED_REPOSITORY' => $this->getRepositoryClassName(),
+            
+            'STORE_VALIDATOR_PHPDOC' => '',
+            'STORE_VALIDATOR_PARAMETER' => '',
+            
+            'STORE_REPOSITORY_CALL' => '$this->' . $resource . '->create($request->all());',
+            'UPDATE_REPOSITORY_CALL' => '$this->' . $resource . '->update($id, $request->all());',
+        ];
+        
         if ($this->option('repository')) {
             $this->imports[] = $this->getRepositoryClassName();
         }
         
-        return (new Stub($this->getStubName(), [
-            'CLASS_NAMESPACE' => $this->getClassNamespace($module),
-            'CLASS' => $this->getControllerName(),
-            'MODULE' => $this->getFullyQualifiedName(),
-            'RESOURCE' => strtolower(str_plural($this->argument($this->argumentName))),
-            'RESOURCE_SINGULAR' => strtolower($this->argument($this->argumentName)),
-            'IMPORTS' => $this->getTemplateImports(),
-            'REPOSITORY' => class_basename($this->getRepositoryClassName()),
-            'NAMESPACED_REPOSITORY' => $this->getRepositoryClassName(),
-        ]))->render();
+        if ($this->usesValidators()) {
+            $storeValidator = $this->getStoreValidatorClass();
+            $updateValidator = $this->getUpdateValidatorClass();
+            
+            $this->imports[] = $storeValidator;
+            $this->imports[] = $updateValidator;
+            
+            $replacements['STORE_VALIDATOR_PHPDOC'] = PHP_EOL . '     * @param \\' . $storeValidator . ' $validator';
+            $replacements['STORE_VALIDATOR_PARAMETER'] = ', ' . class_basename($storeValidator) . ' $validator';
+            $replacements['STORE_REPOSITORY_CALL'] = '$this->' . $resource . '->create($validator->valid());';
+            
+            $replacements['UPDATE_VALIDATOR_PHPDOC'] = PHP_EOL . '     * @param \\' . $updateValidator . ' $validator';
+            $replacements['UPDATE_VALIDATOR_PARAMETER'] = ', ' . class_basename($updateValidator) . ' $validator';
+            $replacements['UPDATE_REPOSITORY_CALL'] = '$this->' . $resource . '->update($id, $validator->valid());';
+        }
+        
+        $replacements['IMPORTS'] = $this->getTemplateImports();
+        
+        return (new Stub($this->getStubName(), $replacements))->render();
     }
     
     /**
@@ -115,22 +141,36 @@ class ControllerCommand extends GeneratorCommand
     }
     
     /**
-     * @return string
-     */
-    protected function getTemplateImports() : string
-    {
-        return collect($this->imports)->map(function($import) {
-            return 'use ' . $import . ';' . PHP_EOL;
-        })->implode('');
-    }
-    
-    /**
      * Get the constructor template.
      *
      * @return string
      */
-    protected function getRepositoryClassName()
+    protected function getRepositoryClassName() : string
     {
         return $this->getModule()->getNamespace() . '\Repositories\\' . $this->getClass() . 'Repository';
+    }
+    
+    /**
+     * @return bool
+     */
+    protected function usesValidators() : bool
+    {
+        return $this->option('validators');
+    }
+    
+    /**
+     * @return string
+     */
+    protected function getStoreValidatorClass() : string
+    {
+        return $this->getModule()->getNamespace() . '\Http\\Validators\\' . $this->getClass() . 'StoreValidator';
+    }
+    
+    /**
+     * @return string
+     */
+    protected function getUpdateValidatorClass() : string
+    {
+        return $this->getModule()->getNamespace() . '\Http\\Validators\\' . $this->getClass() . 'UpdateValidator';
     }
 }
