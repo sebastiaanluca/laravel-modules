c69be598eeeccb4e5fcf9c351262ae163e82daaa
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-10-02 01:27:39 +0200

    Add primary key field to entities

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -4,4 +4,17 @@ namespace Nwidart\Modules\Entities;
 
 interface Entity
 {
+    /**
+     * Get the field that's used as primary key when retrieving and storing entities of this type.
+     *
+     * @return string
+     */
+    public function getKey() : string;
+    
+    /**
+     * Get the value of the primary key field.
+     *
+     * @return integer|string
+     */
+    public function getKeyValue();
 }
